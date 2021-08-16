8a8759a1dd284413036c190676ede7a16f8e3e24
Harshvardhan <67060748+HarshVardhan22@users.noreply.github.com>
2021-08-16 11:39:14 +0530

    +added upcoming palindrome feature +fixed UI

--- src/Components/Main/Main.js
+++ src/Components/Main/Main.js
@@ -5,6 +5,10 @@ const Main = () => {
   const [value, setValue] = useState("");
   const [flag, setFlag] = useState();
   const [format, setFormat] = useState("");
+  const [nextPDate, setNextPDate] = useState("");
+  const [formatStr, setFormatStr] = useState("");
+  const [daysMissed,setDaysMissed] = useState(0);
+  
   const handleDate = (e) => {
     setValue(e.target.value);
   };
@@ -14,36 +18,90 @@ const Main = () => {
     if (reversedString === input) return true;
   };
 
+
+  const nextPalindrome = () => {
+    let temp = new Date(value);
+    let count = 0;
+    temp.setDate(temp.getDate() + 1);
+    while (1) {
+      count++;
+      setDaysMissed(count);
+      let dateString = temp.toISOString().substring(0, 10).replaceAll("-", "");
+      let year = dateString.substring(0, 4);
+      let month = dateString.substring(4, 6);
+      let day = dateString.substring(6, 8);
+      //YYYYMMDD
+      let dateFormat1 = dateString;
+      //ddMMyyyy
+      let dateFormat2 = day + month + year;
+      //MMDDYY
+      let dateFormat3 = month + day + year.substring(2, 4);
+
+      //YYMMDD
+      let dateFormat4 = year.substring(2, 4) + month + day;
+
+      if (isPalindrome(dateFormat1)) {
+        setNextPDate(year + "-" + month + "-" + day);
+        setFormatStr("YYYY-MM-DD");
+        break;
+      } else if (isPalindrome(dateFormat2)) {
+        setNextPDate(day + "-" + month + "-" + year);
+        setFormatStr("DD-MM-YYYY");
+        break;
+      } else if (isPalindrome(dateFormat3)) {
+        setNextPDate(month + "-" + day + "-" + year.substring(2, 4));
+        setFormatStr("MM-DD-YY");
+        break;
+      } else if (isPalindrome(dateFormat4)) {
+        setFormatStr("YY-MM-DD");
+        setNextPDate(year.substring(2, 4) + "-" + month + "-" + day);
+        break;
+      }
+      temp.setDate(temp.getDate() + 1);
+    }
+  };
+
   const checkDate = (e) => {
+  
     e.preventDefault();
     let date = value.replaceAll("-", "");
 
     let year = date.substring(0, 4);
     let month = date.substring(4, 6);
     let day = date.substring(6, 8);
+    //YYYYMMDD
     let dateFormat1 = date;
+    //ddMMyyyy
     let dateFormat2 = day + month + year;
-    let dateFormat3 = month + day + year.substring(0, 2);
-    let dateFormat4 = month.substring(0, 1) + day + year;
+    //MMDDYY
+    let dateFormat3 = month + day + year.substring(2, 4);
+
+    //YYMMDD
+    let dateFormat4 = year.substring(2, 4) + month + day;
 
     if (isPalindrome(dateFormat1)) {
       setFlag(isPalindrome(dateFormat1));
       setFormat(year + "-" + month + "-" + day);
+      setFormatStr("YYYY-MM-DD");
       return;
     } else if (isPalindrome(dateFormat2)) {
       setFlag(isPalindrome(dateFormat2));
       setFormat(day + "-" + month + "-" + year);
+      setFormatStr("DD-MM-YYYY");
       return;
     } else if (isPalindrome(dateFormat3)) {
-      setFlag(isPalindrome(dateFormat2));
-      setFormat(month + "-" + day + "-" + year.substring(0, 2));
+      setFlag(isPalindrome(dateFormat3));
+      setFormat(month + "-" + day + "-" + year.substring(2, 4));
+      setFormatStr("MM-DD-YY");
       return;
     } else if (isPalindrome(dateFormat4)) {
-      setFlag(isPalindrome(dateFormat2));
-      setFormat(month.substring(0, 1) + "-" + day + "-" + year);
+      setFlag(isPalindrome(dateFormat4));
+      setFormat(year.substring(2, 4) + "-" + month + "-" + day);
+      setFormatStr("YY-MM-DD");
       return;
     } else {
       setFlag(false);
+      nextPalindrome();
     }
   };
 
@@ -54,7 +112,11 @@ const Main = () => {
           <form onSubmit={checkDate}>
             <h2>Enter Your Date Of Birth</h2>
             <input type="Date" value={value} onChange={handleDate} required />
-            <button onClick={checkDate} disabled={value === "" ? true : false} style={{opacity:`${value===""?"0.5":"1"}`}}>
+            <button
+              onClick={checkDate}
+              disabled={value === "" ? true : false}
+              style={{ opacity: `${value === "" ? "0.5" : "1"}` }}
+            >
               Check
             </button>
             <p>20-02-2002 is an example of</p>
@@ -83,10 +145,14 @@ const Main = () => {
                 Your BirthDate is{" "}
                 <span className={styles.textColor}>not a Palindrome</span>
               </h2>
+              <h3>Upcoming Palindrome :  <span className={styles.textColorSmall}>{nextPDate}</span></h3>
+      <h3>In Format : {formatStr}</h3>
+      <h3>You missed it by :  <span className={styles.textColorSmall}>{daysMissed} </span>Days</h3>  
             </div>
           </Bounce>
         )}
       </div>
+      
     </div>
   );
 };
